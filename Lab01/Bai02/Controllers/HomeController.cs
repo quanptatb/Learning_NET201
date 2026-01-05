821ade8749594ff2020ed6a5fdaac31789f7b96d
quanptatb <quanptatb01758@gmail.com>
2026-01-05 23:05:15 +0700

    Lab01 Bai02

--- Lab01/Bai02/Controllers/HomeController.cs
+++ Lab01/Bai02/Controllers/HomeController.cs
@@ -1,14 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bai02.Models;
+using System.Runtime.Versioning;
 
 namespace Bai02.Controllers;
 
 public class HomeController : Controller
 {
+    [Route("/")]
+    [HttpGet("/Home/Index")]
     public IActionResult Index()
     {
         return View();
+    }
+    [HttpGet("/Home/ReturnContent")]
+    public IActionResult ReturnContext()
+    {
+        return Content("Hello world.");
+    }
+    [HttpGet("/Home/ReturnJson")]
+    public IActionResult ReturnJson()
+    {
+        var student = new
+        {
+            Id = 1,
+            Name = "Pham Tran Anh Quan",
+            Age = 20
+        };
+        return Json(student);
     }
 
     public IActionResult Privacy()
